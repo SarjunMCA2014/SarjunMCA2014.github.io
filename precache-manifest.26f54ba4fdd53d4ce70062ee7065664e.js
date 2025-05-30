b286b3f1d46a04fcb6dfa313a24a1521e4f7f45d
Mohamed Sarjun <SarjunMCA2014@gmail.com>
2025-05-31 02:53:11 +0530

    Add Daily Input Question Form
    Add Daily Input Question Form

---
+++ precache-manifest.26f54ba4fdd53d4ce70062ee7065664e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81afd447f13f536eb1f5719dd777dcf6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fefc242bcf1f35235147",
+    "url": "/static/js/2.19fd4448.chunk.js"
+  },
+  {
+    "revision": "d466ab9648f013dea081956fc7c904f3",
+    "url": "/static/js/2.19fd4448.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01708c7384d1d988f43a",
+    "url": "/static/js/3.f57f3633.chunk.js"
+  },
+  {
+    "revision": "d3c0a2d31cd641f36257",
+    "url": "/static/js/4.8af86c39.chunk.js"
+  },
+  {
+    "revision": "d544664962a377419e64",
+    "url": "/static/js/5.f158252f.chunk.js"
+  },
+  {
+    "revision": "d60c6bd120d8175c90d1",
+    "url": "/static/js/6.142c068c.chunk.js"
+  },
+  {
+    "revision": "90ee90dd92dab6e1305a",
+    "url": "/static/js/7.2d98a270.chunk.js"
+  },
+  {
+    "revision": "1f58352f6d9b278985b1",
+    "url": "/static/js/main.a58f6c13.chunk.js"
+  },
+  {
+    "revision": "88ad3bcf69eedc0d2fe6",
+    "url": "/static/js/runtime-main.569c21ea.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6014943673624e89b56f1f9e1607897a.js"
+  "/precache-manifest.26f54ba4fdd53d4ce70062ee7065664e.js"
 );
 
 self.addEventListener('message', (event) => {
